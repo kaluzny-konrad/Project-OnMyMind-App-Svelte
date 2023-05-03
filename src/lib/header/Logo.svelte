c11f9695847b75d15587120c7f477ad26ab6676d
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-03 16:07:14 +0200

    new logo first try

--- src/lib/header/Logo.svelte
+++ src/lib/header/Logo.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import Icon from '$lib/icons/Icon.svelte';
-	import QueuePath from '$lib/icons/QueuePath.svelte';
-	import SquaresPath from '$lib/icons/SquaresPath.svelte';
 </script>
 
 <a href="/" class="flex items-center text-gray-800 hover:text-gray-950">
-	<Icon strokeWidth={2.0}><SquaresPath /></Icon>
+	<img src="/logo.svg" alt="OnMyMind" class="h-6 w-6" />
 	<span class="ml-2 text-2xl font-bold">OnMyMind</span>
 </a>
