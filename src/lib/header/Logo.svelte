e5b8dd0e3362cb388d9ceaf2d417761185aab10d
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-04 15:53:01 +0200

    chosen logo

--- src/lib/header/Logo.svelte
+++ src/lib/header/Logo.svelte
@@ -1,5 +1,5 @@
-<a href="/" class="flex items-center text-gray-800">
-	<img src="/logo-gray.svg" alt="OnMyMind" class="h-12" />
+<a href="/" class="flex items-center text-gray-800 hover:text-gray-900">
+	<img src="/logo-color.svg" alt="OnMyMind" class="h-12" />
 	<span class="logo-font ml-3 text-4xl font-bold">OnMyMind</span>
 </a>
 
