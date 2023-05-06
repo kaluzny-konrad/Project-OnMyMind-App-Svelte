b35f89f31de62e15bea6b292c0f025617eefe9b7
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-06 13:45:27 +0200

    about active

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container mx-auto px-4 py-8">
 	<div class="mb-6 flex flex-col items-center justify-center space-y-4">
-		<h1 class="text-4xl font-bold">About Us</h1>
+		<h1 class="text-4xl font-bold">About OnMyMind</h1>
 		<img
 			class="h-60 w-60 rounded-full"
 			src="/images/about-programmer.png"
@@ -8,20 +8,31 @@
 		/>
 	</div>
 	<div class="flex flex-col items-center justify-center">
+		<h2 class="my-4 text-2xl font-bold">O aplikacji OnMyMind</h2>
 		<p class="m-4 text-center text-lg">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
-			bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra
-			justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus
-			et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum,
-			nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus
-			sapien nunc eget odio.
+			OnMyMind to prosty w obsłudze tracker, który umożliwia śledzenie wielu
+			zadań jednocześnie. Dzięki OnMyMind możesz śledzić postępy wszystkich
+			zadań w jednym miejscu i łatwiej zarządzać swoim czasem.
 		</p>
+		<h2 class="my-4 text-2xl font-bold">Jak działa OnMyMind?</h2>
 		<p class="m-4 text-center text-lg">
-			Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-			ac turpis egestas. Maecenas pellentesque elit a justo malesuada, vitae
-			interdum ex posuere. Vestibulum elementum semper rutrum. Nam consequat mi
-			a dui ultricies, sit amet ullamcorper dolor auctor. Vivamus maximus justo
-			eget mauris pulvinar condimentum.
+			Aplikacja umożliwia ustawienie czasomierza w systemie Pomodoro, aby
+			kontrolować czas pracy i przerw. Dodatkowo każde zadanie dodane do
+			OnMyMind wyposażone jest we własny czasomierz, który pozwala kontrolować
+			czas przeznaczony na jego wykonanie. Celem jest lepsze zarządzanie sobą w
+			czasie i organizację w trudnych warunkach.
 		</p>
+		<h2 class="my-4 text-2xl font-bold">O rozwoju aplikacji</h2>
+		<p class="m-4 text-center text-lg">
+			Rozwijam aplikację OnMyMind w celu zwiększenia efektywności mojej pracy, a
+			także dla innych osób pracujących w podobnych warunkach. Staram się stale
+			ulepszać funkcjonalność i wygląd aplikacji, aby zapewnić użytkownikom
+			najlepsze doświadczenia. W ciągu najbliższych miesięcy planuję dodanie
+			nowych funkcjonalności, takich jak:
+		</p>
+		<ul class="m-4 list-inside list-disc text-center text-lg">
+			<li>Możliwość łatwej zmiany kolejności aktywnych zadań.</li>
+			<li>Możliwość oznaczania priorytetu zadań.</li>
+		</ul>
 	</div>
 </div>
