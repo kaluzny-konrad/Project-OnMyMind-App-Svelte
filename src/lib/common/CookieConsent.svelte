3a3f3ddd8d43145bda699a997e6e53377056e400
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-08 19:53:38 +0200

    #1063 cookies

--- src/lib/common/CookieConsent.svelte
+++ src/lib/common/CookieConsent.svelte
@@ -1,38 +1,52 @@
 <script>
-	import { onMount, setContext } from 'svelte';
-	import { get, writable } from 'svelte/store';
+	import { onMount } from 'svelte';
 
 	const COOKIE_CONSENT_KEY = 'cookie_consent';
 
 	let isConsentGiven = false;
 	let isPopupVisible = false;
 
-	const consentStore = writable(false);
-
+	const cookieConsentStore = writable(false);
 	onMount(() => {
 		isConsentGiven = getConsentFromLocalStorage();
-		if (!isConsentGiven) {
+		if (isConsentGiven === null) {
 			isPopupVisible = true;
 		}
 	});
 
 	function getConsentFromLocalStorage() {
-		const storedConsent = localStorage.getItem(COOKIE_CONSENT_KEY);
-		return storedConsent === 'true';
+		return localStorage.getItem(COOKIE_CONSENT_KEY);
 	}
 
 	function handleConsent() {
 		localStorage.setItem(COOKIE_CONSENT_KEY, 'true');
-		consentStore.set(true);
+		cookieConsentStore.set(true);
 		isPopupVisible = false;
 	}
 
-	setContext('consent', consentStore);
+	function handleDisagree() {
+		localStorage.setItem(COOKIE_CONSENT_KEY, 'false');
+		cookieConsentStore.set(false);
+		isPopupVisible = false;
+		deleteCookies();
+	}
+
+	function deleteCookies() {
+		const cookies = document.cookie.split(';');
+		cookies.forEach((cookie) => {
+			const eqPos = cookie.indexOf('=');
+			const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+			document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+		});
+		location.reload();
+	}
+
+	setContext('cookieConsent', cookieConsentStore);
 </script>
 
 {#if isPopupVisible}
-	<div class="fixed bottom-0 left-0 right-0 z-50 bg-white p-4">
-		<div class="mx-auto max-w-md rounded-lg p-4 text-center shadow-lg">
+	<div class="fixed bottom-40 z-50 m-4 rounded-lg bg-white shadow-xl">
+		<div class="mx-auto max-w-xl p-4 text-center">
 			<p class="mb-2 text-sm text-gray-500">
 				This website uses cookies to ensure you get the best experience on our
 				website.
@@ -41,10 +55,7 @@
 				<button class="round-button blue-button" on:click={handleConsent}>
 					I Agree
 				</button>
-				<button
-					class="round-button gray-button"
-					on:click={() => (isPopupVisible = false)}
-				>
+				<button class="round-button gray-button" on:click={handleDisagree}>
 					I Disagree
 				</button>
 			</div>
