8a595f3abbbb141befb28d520d8969a9947cb123
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-10 17:34:45 +0200

    another sol

--- src/routes/api/send-email.js
+++ src/routes/api/send-email.js
@@ -2,7 +2,15 @@ import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey(import.meta.env.VITE_SENDGRID_API_KEY);
 
-export default async function POST(email, message) {
+export async function GET() {
+	return {
+		status: 200,
+		body: { success: true },
+	};
+}
+
+export async function POST(request) {
+	const { email, message } = request.body;
 	const msg = {
 		to: import.meta.env.VITE_EMAIL_ADDRESS,
 		from: import.meta.env.VITE_EMAIL_ADDRESS,
