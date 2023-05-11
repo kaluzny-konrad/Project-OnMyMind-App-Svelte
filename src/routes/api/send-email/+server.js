85c6313e5719167699b894658da6ab434c68b304
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-11 18:50:04 +0200

    #1078 mail regex

--- src/routes/api/send-email/+server.js
+++ src/routes/api/send-email/+server.js
@@ -6,18 +6,19 @@ const cooldown = 1000 * 1; // 1 second
 sgMail.setApiKey(import.meta.env.VITE_SENDGRID_API_KEY);
 
 export async function POST({ request }) {
+	const { mailContent } = await request.json();
+
 	const now = Date.now();
 	if (now - lastSent < cooldown) {
 		return new Response(false);
 	}
 	lastSent = now;
 
-	const { email, message } = await request.json();
 	const msg = {
 		to: import.meta.env.VITE_EMAIL_ADDRESS,
 		from: import.meta.env.VITE_EMAIL_ADDRESS,
 		subject: 'New message from your site',
-		text: `Mail: ${email}. Message: ${message}`,
+		text: mailContent,
 	};
 
 	try {
