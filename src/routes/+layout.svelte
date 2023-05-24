179113a2fa4ef4136deda3a0a5d781d155d38dd1
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-24 08:31:05 +0200

    #1090 add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import Header from '../lib/common/header/Header.svelte';
 	import Footer from '../lib/common/footer/Footer.svelte';
 	import CookieConsent from '../lib/common/cookie/CookieConsent.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
