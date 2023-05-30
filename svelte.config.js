f2c1f7b4825111230f53e0b3bafef3af311b2c70
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-30 19:46:51 +0200

    sol from twitter

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: vercel(),
+	},
+	define: {
+		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
+			process.env.VERCEL_ANALYTICS_ID,
+		),
 	},
 };
 
