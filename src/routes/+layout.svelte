47ed6d1e09b08b4c58e77f61d6e132a4a177df19
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-30 20:34:08 +0200

    prune

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,31 +4,10 @@
 	import Header from '../lib/common/header/Header.svelte';
 	import Footer from '../lib/common/footer/Footer.svelte';
 	import CookieConsent from '../lib/common/cookie/CookieConsent.svelte';
-	import { browser, dev } from '$app/environment';
+	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-	import { onMount } from 'svelte';
 
 	inject({ mode: dev ? 'development' : 'production' });
-
-	import { webVitals } from '../lib/webvitals';
-	import { page } from '$app/stores';
-
-	const analyticsId: string = import.meta.env.VERCEL_ANALYTICS_ID;
-
-	onMount(async () => {
-		const response = await fetch('/speed');
-		const result = await response.json();
-		console.log(result);
-		console.log($page.url.pathname);
-		console.log($page.params);
-		if (browser) {
-			webVitals({
-				path: $page.url.pathname,
-				params: $page.params,
-				analyticsId,
-			});
-		}
-	});
 </script>
 
 <Header />
--- src/routes/speed/+server.ts
+++
@@ -1,7 +0,0 @@
-import { VERCEL_ANALYTICS_ID } from '$env/static/private';
-import { json } from '@sveltejs/kit';
-
-export function GET() {
-	console.log('analytics id :' + VERCEL_ANALYTICS_ID);
-	return json(VERCEL_ANALYTICS_ID);
-}
