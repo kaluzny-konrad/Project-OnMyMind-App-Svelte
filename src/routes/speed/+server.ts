7448bc9069873c693843b26e125968301c770acd
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-30 20:20:28 +0200

    test env v

--- src/routes/speed/+server.ts
+++ src/routes/speed/+server.ts
@@ -1,7 +1,7 @@
-const analyticsId: string = import.meta.env.VERCEL_ANALYTICS_ID;
+import { VERCEL_ANALYTICS_ID } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 export function GET() {
-	console.log('analytics id :' + analyticsId);
-	return json(analyticsId);
+	console.log('analytics id :' + VERCEL_ANALYTICS_ID);
+	return json(VERCEL_ANALYTICS_ID);
 }
