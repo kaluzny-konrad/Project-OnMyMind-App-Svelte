bb77d8982f99d10386efc6c015662eb587dc3a05
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-30 20:04:36 +0200

    def

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess({
-		replace: [
-			[
-				'import.meta.env.VERCEL_ANALYTICS_ID',
-				JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-			],
-		],
-	}),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: vercel(),
+	},
+	define: {
+		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
+			process.env.VERCEL_ANALYTICS_ID,
+		),
 	},
 };
 
