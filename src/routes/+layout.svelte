a8ff47a276353e4b5a78af2951e88fed694e65cc
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-05-30 19:23:33 +0200

    init speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,19 @@
 	import CookieConsent from '../lib/common/cookie/CookieConsent.svelte';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { onMount } from 'svelte';
 
 	inject({ mode: dev ? 'development' : 'production' });
+
+	import { webVitals } from '../lib/webvitals';
+	import { page } from '$app/stores';
+
+	const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID as string;
+	onMount(() => {
+		const path = $page.url.pathname;
+		const params = $page.params;
+		if (analyticsId) webVitals({ path, params, analyticsId });
+	});
 </script>
 
 <Header />
--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		replace: [
+			[
+				'import.meta.env.VERCEL_ANALYTICS_ID',
+				JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+			],
+		],
+	}),
 
 	kit: {
 		adapter: vercel(),
